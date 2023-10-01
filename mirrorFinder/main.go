4b7a75cb8ff8c5360bc7da8a64d849470dc0f356
Joshua Nichols <joshua.nichols5@snhu.edu>
2023-09-30 20:56:17 -0500

    initial commit

---
+++ mirrorFinder/main.go
@@ -0,0 +1,54 @@
+package mirrorFinder
+
+import (
+	"awesomeProject5/mirrors"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+)
+
+func findFastest(urls []string) response {
+	urlChan := make(chan string)
+	latencyChan := make(chan time.Duration)
+	for _, url := range urls {
+		mirrorURL := url
+		go func() {
+			start := time.Now()
+			_, err := http.Get(mirrorURL + "/README")
+			latency := time.Now().Sub(start) / time.Millisecond
+			if err == nil {
+				urlChan <- mirrorURL
+				latencyChan <- latency
+			}
+		}()
+	}
+	return response{<-urlChan, <-latencyChan}
+}
+
+type response struct {
+	FastestURL string        `json:"fastest_url"`
+	Latency    time.Duration `json:"latency"`
+}
+
+func main() {
+	http.HandleFunc("/fatest-mirror", func(w http.ResponseWriter, r *http.Request) {
+		response := findFastest(mirrors.MirrorList)
+		respJSON, _ := json.Marshal(response)
+		w.Header().Set("Content-Type", "application/json")
+		write, err := w.Write(respJSON)
+		if err != nil {
+			return
+		}
+	})
+	port := ":8000"
+	server := &http.Server{
+		Addr:           port,
+		ReadTimeout:    10 * time.Second,
+		WriteTimeout:   10 * time.Second,
+		MaxHeaderBytes: 1 << 20,
+	}
+	fmt.Printf("Starting Server on port %sn", port)
+	log.Fatal(server.ListenAndServe())
+}
